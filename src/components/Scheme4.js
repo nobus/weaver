0adfd8632dce05622b13ca7777fed2fbce00cfde
Maksim Klimin <meganobus@gmail.com>
2016-11-05 13:50:26 +0300

    Add new component: Scheme4

---
+++ src/components/Scheme4.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/Scheme4.css';
+
+class Scheme4 extends Component {
+  render() {
+    return (
+      <div className='Scheme4'>
+        <span>
+          <div className='Threading'>threading</div>
+          <div className='Weave'>weave</div>
+        </span>
+        <span>
+          <div className='TieUp'>tie-up</div>
+          <div className='Treadling'>treadling</div>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Scheme4;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Scheme4 from './components/Scheme4';
 import './styles/index.css';
 
 ReactDOM.render(
-  <App />,
+  <Scheme4 />,
   document.getElementById('root')
 );
