8154f2832776f432bfc36bb9d3bbe0a3a4a60cf1
Maksim Klimin <meganobus@gmail.com>
2016-11-10 16:42:31 +0300

    Make this component more smarter

--- src/components/Scheme4.js
+++ src/components/Scheme4.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import {weaveOn, weaveOff, threadingOn, threadingOff} from '../actions/scheme4';
 import Threading from './Threading';
 import Weave from './Weave';
 import TieUp from './TieUp';
@@ -9,14 +11,20 @@ import '../styles/Scheme4.css';
 
 class Scheme4 extends Component {
   render() {
+    const {dispatch, switcher4} = this.props;
+
     return (
       <div className='Scheme4'>
         <span className='spanRow'>
-          <span className='spanCell'><Threading /></span>
+          <span className='spanCell'>
+            <Threading currentState={switcher4} />
+          </span>
           <span className='spanCell'><TieUp /></span>
         </span>
         <span className='spanRow'>
-          <span className='spanCell'><Weave /></span>
+          <span className='spanCell'>
+            <Weave currentState={switcher4} />
+          </span>
           <span className='spanCell'><Treadling /></span>
         </span>
       </div>
@@ -24,4 +32,8 @@ class Scheme4 extends Component {
   }
 }
 
-export default Scheme4;
+function select(state) {
+  return state;
+}
+
+export default connect(select)(Scheme4);
