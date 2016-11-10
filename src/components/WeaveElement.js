d41fd29fd77c0fa44bbf91862194597dec113d20
Maksim Klimin <meganobus@gmail.com>
2016-11-10 16:43:06 +0300

    Add stupid click handler

--- src/components/WeaveElement.js
+++ src/components/WeaveElement.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import '../styles/WeaveElement.css';
 
 class WeaveElement extends Component {
+  constructor() {
+    super();
+
+    this.state = {red: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
-    return (<div className='WeaveElement'></div>);
+    const style = this.state.red
+      ? "WeaveElement redWeaveElement"
+      : "WeaveElement whiteWeaveElement";
+
+    return (<div onClick={this.handleClick} className={style}></div>);
+  }
+
+  handleClick(e) {
+    if (this.state.red) this.setState({'red': false});
+    else this.setState({'red': true});
   }
 }
 
