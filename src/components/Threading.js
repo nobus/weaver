62982d7a185229101ef0302eae086a38bd339d23
Maksim Klimin <meganobus@gmail.com>
2016-11-12 09:55:30 +0300

    Add method rowNumber and indents generator

--- src/components/Threading.js
+++ src/components/Threading.js
@@ -4,13 +4,22 @@ import ThreadingRow from './ThreadingRow';
 import '../styles/Threading.css';
 
 class Threading extends Component {
+  rowNumber(n) {
+    return (this.props.currentState.weaves.length <= n)
+      ? n
+      : this.props.currentState.weaves.length
+  }
+
   render() {
+    const indents = [];
+
+    for (let i = 0; i < this.rowNumber(4); i++) {
+      indents.push(<ThreadingRow key={i}/>);
+    }
+
     return (
       <div className='Threading'>
-        <ThreadingRow />
-        <ThreadingRow />
-        <ThreadingRow />
-        <ThreadingRow />
+        {indents}
       </div>
     );
   }
