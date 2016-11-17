1a33645cbe3684bc55881859223eb6b1324402b6
Maksim Klimin <meganobus@gmail.com>
2016-11-17 09:57:51 +0300

    Add new actions

--- src/actions/actions4.js
+++ src/actions/actions4.js
@@ -1,4 +1,8 @@
-import {WEAVE_ON, WEAVE_OFF, THREADING_ON, THREADING_OFF, TREADLING_ON, TREADLING_OFF} from '../constants';
+import {
+  WEAVE_ON, WEAVE_OFF,
+  THREADING_ON, THREADING_OFF,
+  TREADLING_ON, TREADLING_OFF,
+  TIE_UP_ON, TIE_UP_OFF} from '../constants';
 
 export function weaveOn(r, c) {
   return {type: WEAVE_ON, row: r, col: c};
@@ -22,4 +26,12 @@ export function treadlingOn(r, c) {
 
 export function treadlingOff(r, c) {
   return {type: TREADLING_OFF, row: r, col: c};
+}
+
+export function tieUpOn(r, c) {
+  return {type: TIE_UP_ON, row: r, col: c};
+}
+
+export function tieUpOff(r, c) {
+  return {type: TIE_UP_OFF, row: r, col: c};
 }
