20ac1c01f50692773f2c3819c19a6db14cb4cba6
Maksim Klimin <meganobus@gmail.com>
2016-11-17 10:19:58 +0300

    Refactoring

--- src/reducers/reducer4.js
+++ src/reducers/reducer4.js
@@ -26,60 +26,82 @@ function initNewState(state) {
   );
 }
 
+function switcherWeaveOn(state, action) {
+  const newState = initNewState(state);
+  newState.numberOfWeaves++;
+
+  if (newState.weaves[action.row] === undefined)
+    newState.weaves[action.row] = [];
+
+  newState.weaves[action.row][action.col] = true;
+  return newState;
+}
+
+function switcherWeaveOff(state, action) {
+  const newState = initNewState(state);
+  newState.numberOfWeaves--;
+
+  if (newState.weaves[action.row] === undefined)
+    newState.weaves[action.row] = [];
+
+  newState.weaves[action.row][action.col] = false;
+  return newState;
+}
+
+function switcherTieUpOn(state, action) {
+  const newState = initNewState(state);
+
+  if (newState.tieUp[action.row] === undefined)
+    newState.tieUp[action.row] = [];
+
+  newState.tieUp[action.row][action.col] = true;
+  return newState;
+}
+
+function switcherTieUpOff(state, action) {
+  const newState = initNewState(state);
+
+  if (newState.tieUp[action.row] === undefined)
+    newState.tieUp[action.row] = [];
+
+  newState.tieUp[action.row][action.col] = false;
+  return newState;
+}
+
+function switcherThreadingOn(state, action) {
+  const newState = initNewState(state);
+  newState.threadings[action.col] = action.row;
+  return newState;
+}
+
+function switcherThreadingOff(state, action) {
+  const newState = initNewState(state);
+  newState.threadings[action.col] = undefined;
+  return newState;
+}
+
+function switcherTreadlingOn(state, action) {
+  const newState = initNewState(state);
+  newState.treadlings[action.col] = action.row;
+  return newState;
+}
+
+function switcherTreadlingOff(state, action) {
+  const newState = initNewState(state);
+  newState.treadlings[action.col] = undefined;
+  return newState;
+}
+
 function switcher4(state, action) {
-  let newState;
-
   switch (action.type) {
-    case WEAVE_ON:
-      newState = initNewState(state);
-      newState.numberOfWeaves++;
-
-      if (newState.weaves[action.row] === undefined)
-        newState.weaves[action.row] = [];
-
-      newState.weaves[action.row][action.col] = true;
-      return newState;
-    case WEAVE_OFF:
-      newState = initNewState(state);
-      newState.numberOfWeaves--;
-
-      if (newState.weaves[action.row] === undefined)
-        newState.weaves[action.row] = [];
-
-      newState.weaves[action.row][action.col] = false;
-      return newState;
-    case TIE_UP_ON:
-      newState = initNewState(state);
-
-      if (newState.tieUp[action.row] === undefined)
-        newState.tieUp[action.row] = [];
-
-      newState.tieUp[action.row][action.col] = true;
-      return newState;
-    case TIE_UP_OFF:
-      newState = initNewState(state);
-
-      if (newState.tieUp[action.row] === undefined)
-        newState.tieUp[action.row] = [];
-
-      newState.tieUp[action.row][action.col] = false;
-      return newState;
-    case THREADING_ON:
-      newState = initNewState(state);
-      newState.threadings[action.col] = action.row;
-      return newState;
-    case THREADING_OFF:
-      newState = initNewState(state);
-      newState.threadings[action.col] = undefined;
-      return newState;
-    case TREADLING_ON:
-      newState = initNewState(state);
-      newState.treadlings[action.col] = action.row;
-      return newState;
-    case TREADLING_OFF:
-      newState = initNewState(state);
-      newState.treadlings[action.col] = undefined;
-      return newState;
+    case WEAVE_ON: return switcherWeaveOn(state, action);
+    case WEAVE_OFF: return switcherWeaveOff(state, action);
+    case TIE_UP_ON: return switcherTieUpOn(state, action);
+    case TIE_UP_OFF: return switcherTieUpOff(state, action);
+    case THREADING_ON: return switcherThreadingOn(state, action);
+    case THREADING_OFF: return switcherThreadingOff(state, action);
+    case TREADLING_ON: return switcherTreadlingOn(state, action);
+    case TREADLING_OFF: return switcherTreadlingOff(state, action);
     default:
       return initialState;
   }
