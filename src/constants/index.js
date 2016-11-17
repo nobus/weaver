a87652bd0df154e7c8d3d4ebebe6d68292999cd5
Maksim Klimin <meganobus@gmail.com>
2016-11-17 09:57:26 +0300

    Add new constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,5 @@ export const THREADING_ON = 'THREADING_ON';
 export const THREADING_OFF = 'THREADING_OFF';
 export const TREADLING_ON = 'TREADLING_ON';
 export const TREADLING_OFF = 'TREADLING_OFF';
+export const TIE_UP_ON = 'TIE_UP_ON';
+export const TIE_UP_OFF = 'TIE_UP_OFF';
