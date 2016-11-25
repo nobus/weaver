95d990fd7bd2bb77e95d6f45be8a2279fe17676f
Maksim Klimin <meganobus@gmail.com>
2016-11-25 16:58:03 +0300

    Created prototype

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,13 +5,39 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class NavBar extends Component {
   render () {
     return (
-      <nav className="navbar navbar-default navbar-form navbar-static-top">
-      <div class="container-fluid">
-        <div className="form-group">
-          <a className="navbar-brand" href="#">Weaver</a>
+      <nav className="navbar navbar-default navbar-static-top">
+        <div className="container-fluid">
+
+          <div className="row">
+
+            <div className="col-xs-2">
+              <div className="input-group">
+                <input type="text" className="form-control" value={16}/>
+                <span className="input-group-addon">Weave size</span>
+              </div>
+            </div>
+
+            <div className="col-xs-2">
+              <div className="input-group">
+                <input type="text" className="form-control" value={8}/>
+                <span className="input-group-addon">Threading size</span>
+              </div>
+            </div>
+
+            <div className="col-xs-2">
+              <div className="input-group">
+                <input type="text" className="form-control" value={8}/>
+                <span className="input-group-addon">Treadling size</span>
+              </div>
+            </div>
+
+            <div className="col-xs-1">
+              <button className="btn btn-default" type="button">OK</button>
+            </div>
+
+          </div>
+
         </div>
-        <button type="submit" className="btn btn-default">Save</button>
-      </div>
       </nav>
     );
   }
