6a086db906fe2cedc927dccda0f2d2a18f4f6a1f
Maksim Klimin <meganobus@gmail.com>
2016-11-28 18:24:10 +0300

    Fix site-layout

--- src/components/SetDialog.js
+++ src/components/SetDialog.js
@@ -10,45 +10,45 @@ class SetDialog extends Component {
         <div className='panel-body'>
 
           <div className='row'>
-            <div className='col-md-1'>Ro</div>
-            <div className='col-md-1'>
+            <div className='col-xs-1'>Ro</div>
+            <div className='col-xs-1'>
               <input type='text' className='form-control' value={16}/>
             </div>
-            <div className='col-md-1'>Ry</div>
-            <div className='col-md-1'>
-              <input type='text' className='form-control' value={16}/>
-            </div>
-          </div>
-
-          <div className='row'>
-            <div className='col-md-1'>Threading size</div>
-            <div className='col-md-1'>
-              <input type='text' className='form-control' value={16}/>
-            </div>
-            <div className='col-md-1'>Treadling size</div>
-            <div className='col-md-1'>
+            <div className='col-xs-1'>Ry</div>
+            <div className='col-xs-1'>
               <input type='text' className='form-control' value={16}/>
             </div>
           </div>
 
           <div className='row'>
-            <div className='col-md-1'>Element size</div>
-            <div className='col-md-1'>
+            <div className='col-xs-1'>Threading size</div>
+            <div className='col-xs-1'>
               <input type='text' className='form-control' value={16}/>
             </div>
-            <div className='col-md-1'></div>
-            <div className='col-md-1'></div>
+            <div className='col-xs-1'>Treadling size</div>
+            <div className='col-xs-1'>
+              <input type='text' className='form-control' value={16}/>
+            </div>
           </div>
 
           <div className='row'>
-            <div className='col-md-1'></div>
-            <div className='col-md-1'></div>
-            <div className='col-md-1'>
-              <button className='btn btn-default' type='button'>Cansel</button>
+            <div className='col-xs-1'>Element size</div>
+            <div className='col-xs-1'>
+              <input type='text' className='form-control' value={16}/>
             </div>
-            <div className='col-md-1'>
+            <div className='col-xs-1'></div>
+            <div className='col-xs-1'></div>
+          </div>
+
+          <div className='row'>
+            <div className='col-xs-1'></div>
+            <div className='col-xs-1'></div>
+            <div className='col-xs-1'></div>
+
+            <div className='col-xs-1'>
               <button className='btn btn-default' type='button'>OK</button>
             </div>
+
           </div>
       </div>
       </div>
