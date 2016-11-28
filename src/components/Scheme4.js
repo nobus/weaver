55c8a39c366c914ba5a07e96a72027c86f35db5f
Maksim Klimin <meganobus@gmail.com>
2016-11-28 18:17:00 +0300

    Refactoring site-layout

--- src/components/Scheme4.js
+++ src/components/Scheme4.js
@@ -13,8 +13,11 @@ import Weave from './Weave';
 import TieUp from './TieUp';
 import Treadling from './Treadling';
 import NavBar from './NavBar';
+import SetDialog from './SetDialog';
 
 import '../styles/Scheme4.css';
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Scheme4 extends Component {
   render() {
@@ -23,40 +26,45 @@ class Scheme4 extends Component {
     return (
       <div>
         <NavBar />
-        <div className='Scheme4'>
 
-          <span className='spanRow'>
-            <span className='spanCell'>
-              <Threading
-                threadingsState={switcher4.threadings}
-                onClick={(row, col) => dispatch(threadingOn(row, col))}
-                offClick={(row, col) => dispatch(threadingOff(row, col))}
-              />
+        <div className='container'>
+          <SetDialog />
+
+          <div className='Scheme4'>
+
+            <span className='spanRow'>
+              <span className='spanCell'>
+                <Threading
+                  threadingsState={switcher4.threadings}
+                  onClick={(row, col) => dispatch(threadingOn(row, col))}
+                  offClick={(row, col) => dispatch(threadingOff(row, col))}
+                />
+              </span>
+              <span className='spanCell'>
+                <TieUp
+                  currentState={switcher4.tieUp}
+                  onClick={(row, col) => dispatch(tieUpOn(row, col))}
+                  offClick={(row, col) => dispatch(tieUpOff(row, col))}
+                />
+              </span>
             </span>
-            <span className='spanCell'>
-              <TieUp
-                currentState={switcher4.tieUp}
-                onClick={(row, col) => dispatch(tieUpOn(row, col))}
-                offClick={(row, col) => dispatch(tieUpOff(row, col))}
-              />
+            <span className='spanRow'>
+              <span className='spanCell'>
+                <Weave
+                  currentState={switcher4}
+                  onClick={(row, col) => dispatch(weaveOn(row, col))}
+                  offClick={(row, col) => dispatch(weaveOff(row, col))}
+                />
+              </span>
+              <span className='spanCell'>
+                <Treadling
+                  treadlingsState={switcher4.treadlings}
+                  onClick={(row, col) => dispatch(treadlingOn(row, col))}
+                  offClick={(row, col) => dispatch(treadlingOff(row, col))}
+                />
+              </span>
             </span>
-          </span>
-          <span className='spanRow'>
-            <span className='spanCell'>
-              <Weave
-                currentState={switcher4}
-                onClick={(row, col) => dispatch(weaveOn(row, col))}
-                offClick={(row, col) => dispatch(weaveOff(row, col))}
-              />
-            </span>
-            <span className='spanCell'>
-              <Treadling
-                treadlingsState={switcher4.treadlings}
-                onClick={(row, col) => dispatch(treadlingOn(row, col))}
-                offClick={(row, col) => dispatch(treadlingOff(row, col))}
-              />
-            </span>
-          </span>
+          </div>
         </div>
       </div>
     );
