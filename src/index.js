d88207498da92f8d9739c010db36091684f77a67
Maksim Klimin <meganobus@gmail.com>
2016-12-04 10:35:21 +0300

    fix bootstrap import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import Scheme4 from './containers/Scheme4';
 import reducer4 from './redux/reducers/reducer4';
