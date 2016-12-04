b40b47e5d8264ef634fc9064a393a34a71bc367c
Maksim Klimin <meganobus@gmail.com>
2016-12-04 18:08:55 +0300

    Added webpack-combine-loaders

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const combineLoaders = require('webpack-combine-loaders');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -48,11 +49,17 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'postcss-loader'
-        ]
+        loader: combineLoaders([
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ])
       }
     ]
   },
