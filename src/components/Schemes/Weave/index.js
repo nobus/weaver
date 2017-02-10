aa16f2046c64592725f102627c85295c5f7593a5
Maksim Klimin <meganobus@gmail.com>
2017-02-10 14:49:54 +0300

    Chadged layout

--- src/components/Schemes/Weave/index.js
+++ src/components/Schemes/Weave/index.js
@@ -9,25 +9,35 @@ class Weave extends Component {
     const {currentState, onClick, offClick} = this.props;
 
     const indents = [];
-    for (let i = 0; i < (currentState.settings.ry ? currentState.settings.ry : 16); i++) {
-      const rowIndents = [];
-      for (let j = 0; j < (currentState.settings.ro ? currentState.settings.ro : 16); j++)
-        rowIndents.push(<WeaveElement
+
+    for (let i = 0; i < (currentState.settings.ro ? currentState.settings.ro : 16); i++) {
+      const colIndents = [];
+      for (let j = 0; j < (currentState.settings.ry ? currentState.settings.ry : 16); j++)
+        colIndents.push(<WeaveElement
           currentState={currentState}
           onClick={onClick}
           offClick={offClick}
-          row={i} col={j} key={j}
+          row={j} col={i} key={j}
         />);
 
       indents.push(
-        <span className='spanRow' key={i}>
-          {rowIndents}
-        </span>
+        <div className='column' key={i}>
+          {colIndents}
+        </div>
       );
     }
 
+    const elementSize = parseInt(currentState.settings.elementSize, 10);
+
+    const style = {
+      width: (currentState.settings.ro * elementSize) + 2,
+      height: (currentState.settings.ry * elementSize) + 2
+    };
+
+    console.log(style);
+
     return (
-      <div className='Weave'>
+      <div className='Weave' style={style}>
         {indents}
       </div>
     );
