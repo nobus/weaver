2e323893c922d788aa620c46b2be3e7a65dfba2c
Maksim Klimin <meganobus@gmail.com>
2017-02-11 09:24:35 +0300

    Add redux-logger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -9,7 +10,12 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import Scheme4 from './containers/Scheme4';
 import reducer4 from './redux/reducers/reducer4';
 
-const store4 = createStore(reducer4);
+const loggerMiddleware = createLogger();
+
+const store4 = createStore(
+  reducer4,
+  applyMiddleware(loggerMiddleware)
+);
 
 store4.subscribe(() => {
   localStorage.setItem(
